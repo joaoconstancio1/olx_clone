886bd4f62ee29b2f0ddcf2a0b555bc920241795f
joao <joaovitorconstancio@hotmail.com>
2021-04-16 10:48:59 -0300

    saving images on parse server

--- lib/repositories/ad_repository.dart
+++ lib/repositories/ad_repository.dart
@@ -1,8 +1,39 @@
+import 'dart:io';
+
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:xlo_mobx/models/ad.dart';
+import 'package:path/path.dart' as path;
+import 'package:xlo_mobx/repositories/parse_errors.dart';
 
-class AdRepository{
-  void save(Ad ad) {
+class AdRepository {
+  Future<void> save(Ad ad) async {
+    final parseImages = await saveImages(ad.images);
+  }
 
+  Future<List<ParseFile>> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+
+    try{
+      for (final image in images) {
+        if (image is File) {
+          final parseFile = ParseFile(image, name: path.basename(image.path));
+          parseFile.save();
+          final response = await parseFile.save();
+          if (!response.success) {
+            return Future.error(ParseErrors.getDescription(response.error.code));
+          }
+          parseImages.add(parseFile);
+        } else {
+          final parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+          parseImages.add(parseFile);
+        }
+        return parseImages;
+      }
+    } catch (e){
+      return Future.error("Falha ao salvar imagens");
+    }
 
   }
 }
