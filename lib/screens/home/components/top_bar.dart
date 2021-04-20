9173aa33f3c79644711ebd6da17366122b5d02c5
joao <joaovitorconstancio@hotmail.com>
2021-04-20 09:11:43 -0300

    selecting category in the filter

--- lib/screens/home/components/top_bar.dart
+++ lib/screens/home/components/top_bar.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:xlo_mobx/screens/home/components/bar_button.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+import 'package:xlo_mobx/screens/category/category_screen.dart';
+import 'package:xlo_mobx/stores/home_store.dart';
+
+import 'bar_button.dart';
 
 class TopBar extends StatelessWidget {
+  final HomeStore homeStore = GetIt.I<HomeStore>();
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        BarButton(
-          label: 'Categorias',
-          decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.grey[400]))),
-          onTap: () {},
-        ),
+        Observer(builder: (_) {
+          return BarButton(
+            label: homeStore.category?.description ?? 'Categorias',
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(color: Colors.grey[400]),
+              ),
+            ),
+            onTap: () async {
+              final category =
+              await Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => CategoryScreen(
+                  showAll: true,
+                  selected: homeStore.category,
+                ),
+              ));
+              if (category != null) homeStore.setCategory(category);
+            },
+          );
+        }),
         BarButton(
           label: 'Filtros',
           decoration: BoxDecoration(
@@ -20,7 +41,8 @@ class TopBar extends StatelessWidget {
               left: BorderSide(color: Colors.grey[400]),
             ),
           ),
-          onTap: () {},
+          onTap: () {
+          },
         ),
       ],
     );
