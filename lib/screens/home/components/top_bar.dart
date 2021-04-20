b62660f8935a60f8a3047707c43b4bbb660b6da9
joao <joaovitorconstancio@hotmail.com>
2021-04-20 13:24:06 -0300

    creating order field

--- lib/screens/home/components/top_bar.dart
+++ lib/screens/home/components/top_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/screens/category/category_screen.dart';
+import 'package:xlo_mobx/screens/filter/filter_screen.dart';
 import 'package:xlo_mobx/stores/home_store.dart';
 
 import 'bar_button.dart';
@@ -42,6 +43,8 @@ class TopBar extends StatelessWidget {
             ),
           ),
           onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => FilterScreen()));
           },
         ),
       ],
